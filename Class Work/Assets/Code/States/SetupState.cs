d0fb302847ed61fa51a3bac2685416bba4cb97dc
Kyoder2 <kyleyoder@Kyles-MacBook-Pro.local>
2015-10-19 19:51:13 -0500

    so many changes
    forgot to save in awhile

---
+++ Class Work/Assets/Code/States/SetupState.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Assets.Code.Interfaces;
+using System.Collections;
+
+namespace Assets.Code.States
+{
+	public class SetupState : IStateBase
+	{
+		private StateManager manager;
+		private Gameobject player;
+		private
+		
+		public SetupState (StateManager managerRef)
+		{
+			manager = managerRef;
+			if(Application.loadedLevelName != "Scene0")
+				Application.LoadLevel("Scene0");
+		}
+		
+		public void StateUpdate ()
+		{
+			if (Input.GetKeyUp (KeyCode.Space))
+			{
+				manager.SwitchState (new PlayStateScene1_1 (manager));
+			}
+		}
+		
+		public void ShowIt ()
+		{
+			Debug.Log ("In SetupState");
+		}
+	}
+}
+
